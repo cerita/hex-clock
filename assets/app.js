f576f4f94c93c69045d0d0818900c59a4a150a48
Cerita <ceritakelly@gmail.com>
2016-11-25 16:38:59 -0600

    Center clock

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,20 @@
-// JavaScript Document
 function hexClock() {
   var time = new Date();
-  
+
   var h = time.getHours();
   var m = time.getMinutes();
   var s = time.getSeconds();
-  
+
   if (h <= 9) { h = "0" + h};
   if (m <= 9) { m = "0" + m};
   if (s <= 9) { s = "0" + s};
-  
-  
+
+
   var clock = "#" + h + ":" + m + ":" + s;
   var bg = "#" + h + m + s;
-  
+
   $('body').css("background-color", bg);
-  
+
   document.getElementById('clock').innerHTML = clock;
   setInterval(hexClock, 1000);
 };
